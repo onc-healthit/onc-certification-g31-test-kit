a15dbbf8bb2f5f8757de97e5e6a18389a45bbd60
Karl Naden <karl@glenviewassociates.com>
2026-09-01 13:36:15 -0400

    only return the order-sign service on discovery responses

--- lib/onc_certification_g31_test_kit/endpoints/g31_cds_services_discovery_handler.rb
+++ lib/onc_certification_g31_test_kit/endpoints/g31_cds_services_discovery_handler.rb
@@ -6,10 +6,13 @@ module ONCCertificationG31TestKit
     def call(env)
       prefetch_subset = env['PATH_INFO'].split('/').include?('prefetch-subset')
 
+      services = JSON.parse(self.class.cds_services(G31Options::CRD_V221_DOTTED, prefetch_subset:))
+      services['services'].select! { |service| service['hook'] == 'order-sign' }
+
       [
         200,
         { 'Content-Type' => 'application/json', 'Access-Control-Allow-Origin' => '*' },
-        [self.class.cds_services(G31Options::CRD_V221_DOTTED, prefetch_subset:)]
+        [services.to_json]
       ]
     end
   end
