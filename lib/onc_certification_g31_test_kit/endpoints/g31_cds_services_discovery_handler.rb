e198f44400099d253c0ef0a55c9c9a8269afacbc
Karl Naden <karl@glenviewassociates.com>
2026-09-03 08:08:52 -0400

    memoize service response

--- lib/onc_certification_g31_test_kit/endpoints/g31_cds_services_discovery_handler.rb
+++ lib/onc_certification_g31_test_kit/endpoints/g31_cds_services_discovery_handler.rb
@@ -3,16 +3,23 @@ require_relative '../g31_options'
 
 module ONCCertificationG31TestKit
   class G31CDSServicesDiscoveryHandler < DaVinciCRDTestKit::CDSServicesDiscoveryHandler
+    def self.cds_services(version = 'v2.0.1', prefetch_subset: false)
+      key = "#{version}_#{prefetch_subset}"
+      cds_services_array[key] ||= begin
+        services = JSON.parse(super)
+        services['services'].select! { |service| service['hook'] == DaVinciCRDTestKit::ORDER_SIGN_TAG }
+
+        services.to_json
+      end
+    end
+
     def call(env)
       prefetch_subset = env['PATH_INFO'].split('/').include?('prefetch-subset')
-
-      services = JSON.parse(self.class.cds_services(G31Options::CRD_V221_DOTTED, prefetch_subset:))
-      services['services'].select! { |service| service['hook'] == 'order-sign' }
 
       [
         200,
         { 'Content-Type' => 'application/json', 'Access-Control-Allow-Origin' => '*' },
-        [services.to_json]
+        [self.class.cds_services(G31Options::CRD_V221_DOTTED, prefetch_subset:)]
       ]
     end
   end
